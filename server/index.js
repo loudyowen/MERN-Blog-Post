2440c92955a589f76afcb3dcd7a898c36b3ef49d
loudyowen <loudyowen027@gmail.com>
2022-07-10 23:08:43 +0700

    env update

--- server/index.js
+++ server/index.js
@@ -3,14 +3,16 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import postRouter from './routes/post.js';
+import dotenv from 'dotenv';
+
 
 const app = express();
 
 app.use(bodyParser.json({limit: "30mb", extend: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extend: true}));
 app.use(cors());
-
-const URL_DB = 'mongodb+srv://owen:aVkaPCtw0sVZUVfP@cluster0.h5mym.mongodb.net/?retryWrites=true&w=majority'
+dotenv.config();
+const URL_DB = `${process.env.DB_URL}`
 const PORT = process.env.PORT || 5000;
 
 app.use('/posts', postRouter)
